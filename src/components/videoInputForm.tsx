d1f46a4ef6a59d7ea07a0f4c13901f5cc6bd978b
Felipe Araujo <devaraujofelipe@gmail.com>
2023-11-04 00:30:42 +0100

    feat: show video preview

--- src/components/videoInputForm.tsx
+++ src/components/videoInputForm.tsx
@@ -1,4 +1,5 @@
 import { FileVideo, Upload } from 'lucide-react';
+import React from 'react';
 
 import { Separator } from './ui/separator';
 import { Label } from './ui/label';
@@ -6,17 +7,52 @@ import { Textarea } from './ui/textarea';
 import { Button } from './ui/button';
 
 export function VideoInputForm() {
+  const [videoFile, setVideoFile] = React.useState<File | null>(null);
+
+  function handleFileSelected(event: React.ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget;
+
+    if (!files) {
+      return;
+    }
+
+    const selectedFile = files[0];
+
+    setVideoFile(selectedFile);
+  }
+
+  const previewUrl = React.useMemo(
+    () => (videoFile ? URL.createObjectURL(videoFile) : null),
+    [videoFile]
+  );
+
   return (
     <form className="space-y-6">
       <label
         htmlFor="video"
-        className="border flex rounded-md aspect-video cursor-pointer border-da\ text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+        className="relative border flex rounded-md aspect-video cursor-pointer border-da\ text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
       >
-        <FileVideo className="h-4 w-4" />
-        Select a video
+        {previewUrl ? (
+          <video
+            className="pointer-events-none absolute inset-0 aspect-video"
+            controls={false}
+            src={previewUrl}
+          />
+        ) : (
+          <>
+            <FileVideo className="h-4 w-4" />
+            Select a video
+          </>
+        )}
       </label>
 
-      <input type="file" id="video" accept="video/mp4" className="sr-only" />
+      <input
+        type="file"
+        id="video"
+        accept="video/mp4"
+        className="sr-only"
+        onChange={handleFileSelected}
+      />
 
       <Separator />
 
